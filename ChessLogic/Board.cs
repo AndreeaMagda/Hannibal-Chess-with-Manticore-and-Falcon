83545c6061748b9fe0e19cc8be17bca975f4ef54
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2024-05-04 15:29:10 +0300

    Initial board setup

---
+++ ChessLogic/Board.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Board
+    {
+        private readonly Piece[,] pieces = new Piece[9, 10];
+
+        public Piece this[int row, int col]
+        {
+            get{ return pieces[row, col]; }
+            set { pieces[row, col] = value;}
+        }
+        public Piece this[Position pos]
+        {
+            get { return this[pos.Row,pos.Column]; }
+            set { this[pos.Row, pos.Column] = value;}
+        }
+
+        public static Board Initial()
+        {
+            Board board=new Board();
+            board.AddStartPieces();
+            return board;
+        }
+
+        private void AddStartPieces()
+        {
+            this[0, 0] = new Rook(Player.Black);
+        }
+    }
+}
