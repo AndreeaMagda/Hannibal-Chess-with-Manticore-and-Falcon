ca8f35896aeba2bc536cc6ebae0802517d2df630
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2024-05-04 15:40:16 +0300

    Board setup and Gamestate
    Setup the initial state of the board

--- ChessLogic/Board.cs
+++ ChessLogic/Board.cs
@@ -32,6 +32,42 @@ namespace ChessLogic
         private void AddStartPieces()
         {
             this[0, 0] = new Rook(Player.Black);
+            this[0, 1]= new Knight(Player.Black);
+            this[0,2]= new Elephant(Player.Black); 
+            this[0,3]=new Bishop(Player.Black);
+            this[0,4]=new Queen(Player.Black);
+            this[0,5]=new King(Player.Black);
+            this[0, 6] = new Bishop(Player.Black);
+            this[0,7]=new Elephant(Player.Black);
+            this[0,8]=new Knight(Player.Black);
+            this[0,9]=new Rook(Player.Black);
+
+
+            this[8, 0] = new Rook(Player.White);
+            this[8, 1] = new Knight(Player.White);
+            this[8, 2] = new Elephant(Player.White);
+            this[8, 3] = new Bishop(Player.White);
+            this[8, 4] = new Queen(Player.White);
+            this[8, 5] = new King(Player.White);
+            this[8, 6] = new Bishop(Player.White);
+            this[8, 7] = new Elephant(Player.White);
+            this[8, 8] = new Knight(Player.White);
+            this[8, 9] = new Rook(Player.White);
+
+            for (int i = 0; i < 8; i++)
+            {
+                this[1, i]=new Pawn(Player.Black);
+                this[7,i]=new Pawn(Player.White);
+            }
+        }
+        public static bool IsInSide(Position pos)
+        {
+            return pos.Row>=0 &&pos.Row<10 && pos.Column>=0 && pos.Column<9;
+        }
+
+        public  bool IsEmpty(Position pos)
+        {
+            return this[pos] == null;
         }
     }
 }
