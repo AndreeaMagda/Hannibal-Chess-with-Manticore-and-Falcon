e86e4781b13a8e3b75ad7b38c1bf34ebe92c2f05
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2024-05-26 22:41:39 +0300

    Add Can Capture Oppoent King

--- ChessLogic/Pieces/Piece.cs
+++ ChessLogic/Pieces/Piece.cs
@@ -15,36 +15,40 @@ namespace ChessLogic
 
         public abstract IEnumerable<Move> GetMoves(Position from, Board board);
 
-        protected IEnumerable<Position> MovePositionInDirection(Position from, Board board,Direction dir)
+        protected IEnumerable<Position> MovePositionInDirection(Position from, Board board, Direction dir)
         {
-            for(Position pos = from + dir; Board.IsInSide(pos); pos += dir)
+            for (Position pos = from + dir; Board.IsInSide(pos); pos += dir)
             {
-                if (board.IsEmpty(pos)){
-                yield return pos;
-                continue;
+                if (board.IsEmpty(pos))
+                {
+                    yield return pos;
+                    continue;
                 }
                 Piece piece = board[pos];
 
-                if(piece.Color != Color)
+                if (piece.Color != Color)
                 {
                     yield return pos;
                 }
-
                 yield break;
 
             }
-
-            
-
         }
 
-        protected IEnumerable<Position> MovePositionsInDirections(Position from, Board board,Direction[] directions)
+        protected IEnumerable<Position> MovePositionsInDirections(Position from, Board board, Direction[] directions)
         {
             return directions.SelectMany(dir => MovePositionInDirection(from, board, dir));
         }
-    
-    
-    
-    
+
+        public virtual bool CanCaptureOpponentKing(Position from,Board board)
+        {
+            return GetMoves(from, board).Any(move =>
+            {
+                Piece piece = board[move.ToPos];
+                return piece!= null && piece.Type == PieceType.King;
+            });
+        }
+
+
     }
 }
