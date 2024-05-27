cb1f7a142f00b4b665ecb9175a3e9a6538459cc1
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2024-05-27 12:06:12 +0300

    Checkmate & Stalemate

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -11,7 +11,9 @@ namespace ChessLogic
         public Board Board {  get; }
         public Player CurrentPlayer { get; private set; }
 
-        public GameState(Player player,Board board)
+        public Result Result { get; private set; } = null;
+
+    public GameState(Player player,Board board)
         {
             CurrentPlayer = player;
             Board = board;
@@ -26,13 +28,43 @@ namespace ChessLogic
             Piece piece = Board[pos];
             IEnumerable<Move> moveCandidates= piece.GetMoves(pos, Board);
             return moveCandidates.Where(move => move.IsLegal(Board));
-
         }
 
         public void MakeMove(Move move)
         {
             move.Execute(Board);
             CurrentPlayer = CurrentPlayer.Opponent();
+            CheckForGameOver();
+        }
+
+        public IEnumerable<Move> AllLegalMovesFor(Player player)
+        {
+            IEnumerable<Move> moveCandidates = Board.PiecePositionsFor(player).SelectMany(pos =>
+            {
+                Piece piece = Board[pos];
+                return piece.GetMoves(pos, Board);
+            });
+            return moveCandidates.Where(move=>move.IsLegal(Board));
+        }
+
+        private void CheckForGameOver()
+        {
+            if(!AllLegalMovesFor(CurrentPlayer).Any())
+            {
+                if(Board.IsInCheck(CurrentPlayer))
+                {
+                    Result = Result.Win(CurrentPlayer.Opponent());
+                }
+                else
+                {
+                    Result = Result.Draw(EndReason.Stalemate);
+                }
+            }
+        }
+
+        public bool IsGameOver()
+        {
+            return Result != null;
         }
     }
 }
