f64a2ac003fb79ce134a027ac90060d831321edc
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2024-05-28 14:38:36 +0300

    Make menu usable

--- ChessUI/PromotionMenu.xaml.cs
+++ ChessUI/PromotionMenu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,36 @@ namespace ChessUI
     /// </summary>
     public partial class PromotionMenu : UserControl
     {
-        public PromotionMenu()
+        public event Action<PieceType> OptionSelected;
+
+        public PromotionMenu(Player player)
         {
             InitializeComponent();
+
+            QueenImg.Source = Images.GetImage(player, PieceType.Queen);
+            RookImg.Source = Images.GetImage(player, PieceType.Rook);
+            BishopImg.Source = Images.GetImage(player, PieceType.Bishop);
+            KnightImg.Source = Images.GetImage(player, PieceType.Knight);
+        }
+
+        private void QueenImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OptionSelected?.Invoke(PieceType.Queen);
+        }
+
+        private void RookImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OptionSelected?.Invoke(PieceType.Rook);
+        }
+
+        private void BishopImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OptionSelected?.Invoke(PieceType.Bishop);
+        }
+
+        private void KnightImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OptionSelected?.Invoke(PieceType.Knight);
         }
     }
 }
